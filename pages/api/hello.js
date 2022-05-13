55b2dd3d8c763598b7067461ac4cacb6ee27690a
Adam Horodyski <ad.horodyski@gmail.com>
2022-05-13 12:27:34 +0200

    feat: toggl client

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import ToggleClient from "toggl-api";
+
+const togglClient = new ToggleClient({
+  apiToken: process.env.NEXT_PUBLIC_TOGGL_API_TOKEN,
+});
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  togglClient.startTimeEntry(
+    {
+      description: "Some cool work",
+      billable: true,
+    },
+    (err, timeEntry) => {
+      // working now exactly 20 seconds
+      setTimeout(function () {
+        toggl.stopTimeEntry(timeEntry.id, function (err) {
+          // handle error
+
+          toggl.updateTimeEntry(
+            timeEntry.id,
+            { tags: ["finished"] },
+            function (err) {
+              toggl.destroy();
+            }
+          );
+        });
+      }, 20_000);
+    }
+  );
+
+  res.status(200).json({ name: "John Doe" });
 }
