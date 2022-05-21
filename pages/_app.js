d742eb9f116e76b5d2b080fd9a05c7a41007c5eb
Adam Horodyski <ad.horodyski@gmail.com>
2022-05-22 01:31:38 +0200

    feat: basic ui for choosing toggl workspace

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "lib/queryClient";
+
+const App = ({ Component, pageProps }) => (
+  <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
+);
 
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import { useAtom } from "jotai";
+import { useTogglWorkspaces } from "lib/queries";
+import { workspaceAtom } from "lib/atoms";
 
-const Page = () => (
-  <div>
-    <Head>
-      <title>Toggl + Primetric</title>
-    </Head>
-    <p>whatever</p>
-  </div>
-);
+const Page = () => {
+  const { data: workspaces } = useTogglWorkspaces();
+  const [, setWorkspace] = useAtom(workspaceAtom);
+
+  return (
+    <div>
+      <Head>
+        <title>Toggl + Primetric</title>
+      </Head>
+      <div>
+        <h2>Choose your Toggl workspace</h2>
+        {workspaces?.map((workspace) => (
+          <button key={workspace.id} onClick={() => setWorkspace(workspace)}>
+            {workspace.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Page;
